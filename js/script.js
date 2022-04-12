4357be6c6deac50ddf11f940cb2355ffd8f0dc98
Ashley Cove <ash.cove99@gmail.com>
2022-04-12 19:11:40 +0100

    Added function to change images when thumbnail clicked, added class to style thumbnail when selected and hovered

--- js/script.js
+++ js/script.js
@@ -4,6 +4,21 @@ const cartQuantity = document.querySelector('.quantity-cart');
 const addQuantity = document.querySelector('.plus-btn');
 const subtractQuantity = document.querySelector('.minus-btn');
 
+const mainImage = document.querySelector('.product-image-main');
+const imageThumbs = document.querySelectorAll('.image-thumb');
+
+console.log(mainImage.src);
+
+// Function to update images when thumbnail clicked
+imageThumbs.forEach(img => {
+  img.addEventListener('click', e => {
+    const imgStart = img.src.lastIndexOf('/') + 1;
+    const imgEnd = img.src.lastIndexOf('-');
+
+    mainImage.src = 'images/' + img.src.slice(imgStart, imgEnd) + '.jpg';
+    console.log(img.src.slice(imgStart, imgEnd));
+  });
+});
 let quantity = +cartQuantity.textContent;
 
 // eventlisteners to add & subtract amount
