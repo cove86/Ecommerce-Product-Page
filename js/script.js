bbf298ff3498f6b021a6f1d2a78c4aca27c3a980
Ashley Cove <ash.cove99@gmail.com>
2022-05-01 14:23:20 +0100

    Added various stylings

--- js/script.js
+++ js/script.js
@@ -5,12 +5,16 @@ const addQuantity = document.querySelector('.plus-btn');
 const subtractQuantity = document.querySelector('.minus-btn');
 
 const productImageDiv = document.querySelector('.product-image');
+const productImageModal = document.querySelector('.product-image-modal');
 const mainImage = document.querySelector('.product-image-main');
 const imageThumbs = document.querySelectorAll('.image-thumb');
 
+const mainImageModal = document.querySelector('.product-image-main-modal');
+const imageThumbsModal = document.querySelectorAll('.image-thumb-modal');
+
 productImageDiv.addEventListener('click', e => {
-  productImageDiv.classList.remove('hidden');
-  productImageDiv.classList.add('overlay');
+  productImageModal.classList.remove('hidden');
+  // productImageModal.classList.add('overlay');
 });
 
 // Function to update images when thumbnail clicked
@@ -23,6 +27,17 @@ imageThumbs.forEach(img => {
     console.log(img.src.slice(imgStart, imgEnd));
   });
 });
+
+imageThumbsModal.forEach(img => {
+  img.addEventListener('click', e => {
+    const imgStart = img.src.lastIndexOf('/') + 1;
+    const imgEnd = img.src.lastIndexOf('-');
+
+    mainImageModal.src = 'images/' + img.src.slice(imgStart, imgEnd) + '.jpg';
+    console.log(img.src.slice(imgStart, imgEnd));
+  });
+});
+
 let quantity = +cartQuantity.textContent;
 
 // eventlisteners to add & subtract amount
